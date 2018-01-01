0278ef910bd51b5b71a3044de41fdfb43c6d9c4b
LAPTOP-CFCK7EUH\77381 <773815862@qq.com>
2018-01-01 23:49:55 +0800

    博客网站从新跟新

---
+++ js/index/index.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="../../node_modules/_angular@1.6.7@angular/angular.min.js" />
+(function (angular) {
+    var myApp = angular.module('myApp', []);
+    myApp.controller('index', ['$scope', function ($scope) {
+        $scope.logoFor = false;
+        $scope.ohterSignin = function () {
+            if ($scope.logoFor) {
+                $scope.logoFor = false;
+            }
+            else {
+                $scope.logoFor = true;
+            }
+            
+        }
+    }])
+})(angular)
